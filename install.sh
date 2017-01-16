74d4fbf499afe3e8a6eff728c5ea04525883ad42
Abhinav Tushar <abhinav.tushar.vs@gmail.com>
2017-01-16 06:41:41 -0500

    Init

---
+++ install.sh
@@ -0,0 +1,41 @@
+#!/usr/bin/env bash
+
+REMOTE=${REMOTE:-git@github.com:lepisma/pkr.git}
+DEST=${DEST:-/tmp/pkr}
+
+# List of binaries
+BINS=( pkr )
+
+# Change PREFIX to binary path
+[ -z $PREFIX ] && PREFIX=/usr/local/bin
+
+install () {
+    uninstall
+    echo "Installing to $PREFIX"
+    cd $PREFIX
+    for bin in $BINS
+    do
+        cp "$DEST/bin/$bin" ./
+    done
+    return $?
+}
+
+uninstall () {
+    echo -e "\nUninstalling any previous version..."
+    cd $PREFIX
+    for bin in $BINS
+    do
+        [ -e $bin ] && rm $bin
+    done
+    return $?
+}
+
+# Main entry point
+rm -rf "${DEST}"
+
+echo "Downloading latest version..."
+git clone --depth=1 "${REMOTE}" "${DEST}" > /dev/null 2>&1
+
+install
+
+echo "Done"
